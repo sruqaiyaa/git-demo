3f399f00ed2d6c5ef03ee1bb1b9c3e8666c9e6cf
sruqaiyaa@gmail.com <ruqaiyaa.ruqaiyaa@capgemini.com>
2024-03-21 10:25:46 +0530

    hi

---
+++ ArraysTo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Scanner;
+public class ArraysTo {
+
+	public static void main(String[] args) {
+		String[] arr= {"banana","mango","apple","orange"};
+		List<String>list=new ArrayList<String>();
+		for(int i=0;i<arr.length;i++) {
+			list.add(arr[i]);
+		}
+		System.out.println(list);
+		SortedSet<String> set=new TreeSet<String>();
+		list.addAll(set);
+		for(int i=0;i<list.size();i++) {
+			set.add(list.get(i));
+		}
+		System.out.println(set);
+	}
+
+}
