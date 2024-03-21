5976f81eb7c68fcd14aaf33257d6d0ee0df0fef0
sruqaiyaa@gmail.com <ruqaiyaa.ruqaiyaa@capgemini.com>
2024-03-21 10:34:39 +0530

    hi

--- ArraysTo.java
+++ ArraysTo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ArraysTo {
 
 	public static void main(String[] args) {
-		String[] arr= {"banana","mango","apple","orange"};
+		String[] arr= {"banana","mango","apple","orange","avacado"};
 		List<String>list=new ArrayList<String>();
 		for(int i=0;i<arr.length;i++) {
 			list.add(arr[i]);
